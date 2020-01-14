8dd4b4f42cd289b7eb426f0a8a84d4734a25e8b5
starc@qq.com <starc@qq.com>
2020-01-14 23:41:04 +0800

    AttachHandler

--- src/main/java/com/github/vlmap/spring/loadbalancer/core/platform/reactive/GrayAttachReactiveWebFilter.java
+++ src/main/java/com/github/vlmap/spring/loadbalancer/core/platform/reactive/GrayAttachReactiveWebFilter.java
@@ -1,31 +1,20 @@
 package com.github.vlmap.spring.loadbalancer.core.platform.reactive;
 
 import com.github.vlmap.spring.loadbalancer.GrayLoadBalancerProperties;
+import com.github.vlmap.spring.loadbalancer.core.AttachHandler;
 import com.github.vlmap.spring.loadbalancer.core.cli.GaryAttachParamater;
-import com.github.vlmap.spring.loadbalancer.core.cli.GrayAttachCommandLineParser;
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.MapUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.context.properties.bind.BindResult;
-import org.springframework.boot.context.properties.bind.Bindable;
-import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.boot.web.reactive.filter.OrderedWebFilter;
-import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
-import org.springframework.context.event.EventListener;
 import org.springframework.core.Ordered;
-import org.springframework.core.env.Environment;
-import org.springframework.http.HttpCookie;
-import org.springframework.http.HttpHeaders;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilterChain;
 import reactor.core.publisher.Mono;
 
-import java.util.*;
-import java.util.regex.Pattern;
+import java.util.List;
 
 public class GrayAttachReactiveWebFilter implements OrderedWebFilter {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -33,117 +22,40 @@ public class GrayAttachReactiveWebFilter implements OrderedWebFilter {
     private GrayLoadBalancerProperties properties;
 
 
-    private Environment environment;
-    List<GaryAttachParamater> attachs = Collections.emptyList();
+    AttachHandler attachHandler;
 
-    public GrayAttachReactiveWebFilter(GrayLoadBalancerProperties properties, Environment environment) {
+    public GrayAttachReactiveWebFilter(GrayLoadBalancerProperties properties, AttachHandler attachHandler) {
 
         this.properties = properties;
-        this.environment = environment;
-    }
-
-    String key = "vlmap.spring.loadbalancer.attach.el[0]";
-
-
-    private static final Pattern QUERY_PATTERN = Pattern.compile("(vlmap.gray.attch[^&=]+)(=?)([^&]+)?");
-
-    GrayAttachCommandLineParser parser = new GrayAttachCommandLineParser();
-
-    @EventListener(EnvironmentChangeEvent.class)
-    public void listener(EnvironmentChangeEvent event) {
-        List<GaryAttachParamater> list = new ArrayList<>();
-        Set<String> keys = event.getKeys();
-
-
-        BindResult<List<String>> result = Binder.get(environment).bind("vlmap.spring.loadbalancer.attach.el", Bindable.listOf(String.class));
-        List<String> expressions = result.orElse(null);
-
-        if (CollectionUtils.isNotEmpty(expressions)) {
-            for (String expression : expressions) {
-
-                GaryAttachParamater paramater = parser.parser(expression);
-                if (paramater != null) {
-                    list.add(paramater);
-                }
-
-
-            }
-        }
-
-
-        this.attachs = list;
+        this.attachHandler = attachHandler;
 
     }
-
-
-    protected String attach(GaryAttachParamater paramater, ServerWebExchange exchange) {
-        boolean strict = paramater.isStrict();
-
-
-    }
-
-    protected String allAttach(GaryAttachParamater paramater, ServerWebExchange exchange) {
-        Map<String, String> map = paramater.getCookies();
-
-        if (MapUtils.isNotEmpty(map)) {
-            MultiValueMap<String, HttpCookie> cookies = exchange.getRequest().getCookies();
-
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                List<HttpCookie> list = cookies.get(entry.getKey());
-                String value = list.stream().filter(cookie -> StringUtils.equals(cookie.getValue(), entry.getValue())).map(cookie -> cookie.getValue()).findFirst().orElse(null);
-                if (value == null) {
-                    return null;
-                }
-            }
-
-
-        }
-        map = paramater.getHeaders();
-
-        if (MapUtils.isNotEmpty(map)) {
-            HttpHeaders headers = exchange.getRequest().getHeaders();
-
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-
-                List<String> list = headers.get(entry.getKey());
-                if(CollectionUtils.isNotEmpty(list)){
-                    if(!list.contains(entry.getValue())) {
-                        return null;
-                    }
-                }else {
-                    return null;
-                }
-
-            }
-
-
-        }
-        List<String> list= paramater.getMethods();
-        if(CollectionUtils.isNotEmpty(list)){
-            if(!list.contains(exchange.getRequest().getMethod().name())){
-                return null;
-            }
-        }
-
-        Map<String, String> params= paramater.getParams();
-        if(MapUtils.isNotEmpty(params)){
-            MultiValueMap<String,String> _params=new LinkedMultiValueMap();
-            _params.addAll(exchange.getRequest().getQueryParams());
-
-        }
-
-
-        return   null;
-    }
-
-    protected boolean container()
 
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
 
+        List<GaryAttachParamater> paramaters = attachHandler.getAttachParamaters();
+        if (CollectionUtils.isNotEmpty(paramaters)) {
 
-        return chain.filter(exchange);
+          return  Mono.from( AttachHandler.parser(paramaters, new AttachHandler.SimpleRequestData(), exchange)).flatMap(data -> {
+                ServerHttpRequest.Builder builder=  exchange.getRequest().mutate();
+                for (GaryAttachParamater paramater : paramaters) {
+                    String value = attachHandler.attach(paramater, data);
+                    if (StringUtils.isNotBlank(value)) {
+                        builder.header(properties.getHeaderName(), value);
+
+                    }
+                }
+                ServerWebExchange object= exchange.mutate().request( builder.build()).build() ;
+                return chain.filter(object);
+            });
+
+
+        } else {
+            return chain.filter(exchange);
+
+        }
     }
 
 
